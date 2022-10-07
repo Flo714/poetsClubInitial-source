6b59ee80048ca531703811f0c6cfdabf13637cba
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2022-10-07 17:05:00 +0200

    bon comit avec .gitignore

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,12 @@ import { SupabaseAuthClient } from '@supabase/supabase-js/dist/module/lib/Supaba
     <div class="hidden" id="addPoem">
       <div><SignIn msg="Write your poem !" /></div>
       <h3>The poem remains private, until you make it public</h3>
+      <select required name="Language" v-model="Language" >
+              <option>Français</option>
+              <option>Anglais</option>
+              <option>Espagnol</option>
+              <option>Germagnynois</option>
+      </select>
       <label>Poem's title</label><br>
 	      <input type="text" required name="title" v-model="title" placeholder="edit me"><br>
 	      <label>Poem's content</label><br>
@@ -31,10 +37,14 @@ import { SupabaseAuthClient } from '@supabase/supabase-js/dist/module/lib/Supaba
         
 	      <input type="file"  id="file" name="file" placeholder="my file" accept="image/png, image/jpeg"><br>
 	      <!--<img id="illustration" src="./assets/null.png" alt="poem illustration" width="75" height="75"/><br>-->
-        <input type="checkbox" v-model="hidden" value=true/>
+        <input type="checkbox" id="checkbox" v-model="hidden" checked/>
         <label>Hidden poem</label>
       <br><button v-on:click="createPoem()">Add the poem</button>
       <button v-on:click="fetchPoems()">List of poems</button><br>
+      <div>
+          <input type="text" v-model="mot">
+          <button v-on:click="filtrer()">Recherche</button>
+        </div>
       <label for="poemtitle" id="poemtitle" style="color: teal;font-weight: 500;"> ... </label> 
       <img id="poemillustration" src="./assets/null.jpg" alt="poem illustration" width="75" height="75" style="background-color:gray;"/><br>
       <textarea  id="poemcontent" readonly rows="10" cols="50"> ... </textarea> <br>
@@ -51,38 +61,136 @@ import { SupabaseAuthClient } from '@supabase/supabase-js/dist/module/lib/Supaba
 
 <script>
 
-const SUPABASE_URL = 'YOUR_SUPABASE_URL'
-const SUPABASE_KEY = 'ANON_SUPABASE_KEY'
+const SUPABASE_URL = 'https://bzciqcbnmsgtsuflbcjy.supabase.co'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6Y2lxY2JubXNndHN1ZmxiY2p5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjMzMzcyNDEsImV4cCI6MTk3ODkxMzI0MX0.b52EyXMgTnXNPGjWmEKp5z34Ezx31LVHgbDLTh8kvb8'
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
+
+var poemsList  //list of selected poems readabe by the current user
+var currentpoem   //the currentely displayed poem that the user consults
 
 export default {
   methods: {  
     //this method allows a new user to sign up the system. Once done, the user receives an email
     //asking for account validation. Once the validation made the user is added to the system
     async register(){
-
+      //manage exception
+       try {
+        //call the sign up procedure using the email and the password
+        //entred by the user
+        const { user, session, error } = await supabase.auth.signUp({
+          email: this.email,
+          password: this.passwd
+        })
+        if (error) throw error;
+      } catch (error) {
+        alert(error.error_description || error.message);
+      }
     },
     //this method allows the already registred user to log in the system.
     //only authenticated users can later add or read the poems
     async login(){
-
+      //manage potential exception
+     try {
+      //run the procedure of signing in with existant account 
+        const { user, session, error } = await supabase.auth.signIn({
+          email: this.email,
+          password: this.passwd
+        })
+        if (error) throw error;
+        //if the login in succeed, the user is prompted to add new poem
+        //or to consult the existant poems
+        else {
+          document.getElementById('signOut').style.visibility='hidden'
+          document.getElementById('addPoem').style.visibility='visible'
+        }
+      } catch (error) {
+        alert(error.error_description || error.message);
+      }
     },
     //this method allows to add new poem for the authenticated user (after sign in) 
     //it is called when the user click on the add poem button after being entered
     //the title, the content, the visibility and the associated illustration
     async createPoem(){
-    
+      var res;
+      
+        //insert a new illustration file to the poems illustration bucket called: poemsillustrations
+        //the name of the file is a concatination of the user id and the uploaded file name
+        //this way, different users may upload files with the same name 
+        const { data: objects, error } = await supabase.storage
+          .from('images')
+          .upload(supabase.auth.user().id+"_"+file.files[0].name, file.files[0])
+        
+        //extract the url of the added file
+        //this url is added as a field of the poem entry to add
+        res = supabase.storage
+          .from('images')
+          .getPublicUrl(supabase.auth.user().id+"_"+file.files[0].name).data.publicURL;
+               
+        //insert a new raw in poems' table based on the entered data : title, content and hidden status
+        //P.S. the email of the author is automatically added since the email column is declarated
+        //with a default value auth.email(). The user cannot associate its poem to another author !
+        if(document.getElementById("checkbox").checked) this.hidden=true
+        else this.hidden=false
+        alert(this.hidden + " "+ this.email + " "+ this.title + " " +this.content + " " +res) 
+        await supabase
+            .from('poems')
+            .insert([
+            { hidden: this.hidden, email:this.email, title: this.title, content: this.content, illustrationurl: res, Language:this.Language} ])
+       
+
     },
     //this method allows to extract all readable poems of the authenticated user
     //including his peoms and the not hidden poems. This policy is implemented by the supabase system 
     async fetchPoems(){
-        
+      //mange supabase access exceptions
+        try{
+        //select all accessible poems (owned poems or public ones) 
+        const { data, error } = await supabase
+          .from('poems')
+          .select()
+        poemsList=data
+        if (error) throw error;
+        //display the first accessible poem if there is at least one poem
+        if(data.length>0){
+            document.getElementById('poemtitle').innerHTML=data[0].title+"    "
+            document.getElementById('poemcontent').value=data[0].content
+            document.getElementById('poemillustration').src=data[0].illustrationurl
+        }
+        //store the indexof the currently displayed poem
+        currentpoem=0;
+      } catch (error) {
+        alert(error.error_description || error.message);
+      }    
     },
+    async filtrer(){
+      try{
+        const { data, error } = await supabase
+        .from('poems')
+        .select()
+        .like('title','%'+this.mot+'%')
+        poemsList=data
+        if(data.length>0){
+            document.getElementById('poemtitle').innerHTML=data[0].title+"    "
+            document.getElementById('poemcontent').value=data[0].content
+            document.getElementById('poemillustration').src=data[0].illustrationurl
+        }
+        currentpoem=0;
+            if (error) throw error;
+            }
+            catch (error) {
+              alert(error.error_description || error.message);
+            }
+    }, 
     //this function allows to display the next accessibe poem for the current user
     //the fetch button should be selected before
     nextPoem(){
-    
+      if(currentpoem<poemsList.length-1) {
+          currentpoem++
+          document.getElementById('poemtitle').innerHTML=poemsList[currentpoem].title+"    "
+          document.getElementById('poemcontent').value=poemsList[currentpoem].content
+          document.getElementById('poemillustration').src=poemsList[currentpoem].illustrationurl
+        }
     }
   }  
 }
